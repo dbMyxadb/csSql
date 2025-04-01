a14ced56221dff3a369022a90b8b77e7f845cc35
Владислав <@DESKTOP-FB9IMOE>
2025-04-01 19:11:30 +0300

    Add project files.

---
+++ csSql/Program.cs
@@ -0,0 +1,19 @@
+﻿using csSql;
+using Microsoft.EntityFrameworkCore;
+using csSql.Entities;
+
+namespace csSql
+{
+    public class AppDbContext : DbContext
+    {
+        private string _connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=UN;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+
+
+    }
+}
